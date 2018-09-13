3ed32ac82ada2d82d6a6bf5775d3d6de411f9df0
JuliaBichler <julia.bichler@students.htlinn.ac.at>
2018-09-13 16:32:09 +0200

    Inital commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    for(int i=1; i<=100; i++)
+        {
+            System.out.print(i+": ");
+            if((i%3)==0)
+            {
+                System.out.print("Fis");
+            }
+            if((i%4)==0)
+            {
+                System.out.print("Pass");
+            }
+            if(((i%3)!=0)&&((i%4)!=0))
+            {
+                System.out.print(i);
+
+            }
+            System.out.println();
+
+        }
+    }
+}
