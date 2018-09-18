48ee23775db332b775ef702531d5f42239bc588a
JuliaBichler <julia.bichler@students.htlinn.ac.at>
2018-09-18 10:14:33 +0200

    Fertig

---
+++ Sortierung/src/com/company/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int speicher;
+
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        a.add(8); a.add(7); a.add(4); a.add(10);
+        a.add(3); a.add(17); a.add(11); a.add(-4);
+
+        for(int i=1; i<a.size(); i++) {
+            for(int j=0; j<a.size()-i; j++) {
+                if(a.get(j)>a.get(j+1)) {
+                    speicher=a.get(j);
+                    a.set(j,a.get(j+1));
+                    a.set(j+1, speicher);
+                }
+            }
+        }
+        for (int i = 0; i<a.size(); i++) {
+            System.out.print(a.get(i) + ", ");
+        }
+
+
+
+    }
+}
